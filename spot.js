4bee1438509000da8f1d9bb0ec97e59327230d0a
Luke Gartland <63882666+scubaluke@users.noreply.github.com>
2020-11-16 17:52:07 -1000

    removed case sensitivity

--- spot.js
+++ spot.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('.check')
 
 function checkResult() {
-    const feeling = document.querySelector('#feeling').value;
+    const feeling = document.querySelector('#feeling').value.toLowerCase();
     if(feeling === 'fear') {
         document.querySelector('.correct').classList.add('show')
     }
